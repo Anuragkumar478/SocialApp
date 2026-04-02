1a5b7033c10217ce308d1babcf69ac4647309668
Anurag kumar <kumaranurag9795@gmail.com>
2026-04-03 00:08:11 +0530

    change some file on the production level in auth

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -2,13 +2,13 @@ import jwt from "jsonwebtoken";
 
 const generateToken = (res, userId) => {
   const token = jwt.sign({ id: userId }, process.env.JWT_SECRET, {
-    expiresIn: "7d"
+    expiresIn: "7d",
   });
 
   res.cookie("token", token, {
-    httpOnly: true,     // prevent XSS
-    secure: false,      // true in production (https)
-    sameSite: "strict",
+    httpOnly: true,               // prevent XSS
+    secure: process.env.NODE_ENV === "production", // HTTPS only in prod
+    sameSite: "None",              // allows cross-origin cookies
     maxAge: 7 * 24 * 60 * 60 * 1000
   });
 };
